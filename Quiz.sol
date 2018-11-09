f3f4650dddc5838bd4805368717604b6df232493
shipta-golechha <32227907+shipta-golechha@users.noreply.github.com>
2018-11-09 10:17:25 +0530

    Create Quiz.sol

---
+++ Quiz.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.24;
+
+    contract quiz{
+        address player1;
+        address player2;
+        uint [3][3] board;
+        uint turn;
+        uint playerCount;
+        
+    }

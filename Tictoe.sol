28c342710cc21b0050e1bcf465ff707ea16f4275
Parul Jain <32247098+paruljain02@users.noreply.github.com>
2018-11-09 10:44:42 +0530

    Update Tictoe.sol

--- Tictoe.sol
+++ Tictoe.sol
@@ -8,12 +8,13 @@ contract Tictoe
     uint[3][3] game;
     uint turn;
     uint noofplayers;
-    
+    uint noofturns;
     
     constructor(){
         organiser = msg.sender;
         turn = 0;
         noofplayers = 0;
+        noofturns = 0;
         for (uint i = 0;i < 3; i++)
         {
             for (uint j = 0;j<3;j++)
@@ -41,16 +42,74 @@ contract Tictoe
         }
     }
     
-    function play_game() public number_of_players(){
+    
+    function rowCrossed() returns (bool)
+   {
+       for (int i=0; i<3; i++)
+       {
+           if (game[i][0] == game[i][1] &&
+               game[i][1] == game[i][2])
+               return (true);
+       }
+       return(false);
+   }
+    
+   function columnCrossed() returns (bool)
+   {
+       for (int i=0; i<3; i++)
+       {
+           if (game[0][i] == game[1][i] &&
+               game[1][i] == game[2][i])
+               return (true);
+       }
+       return(false);
+   }
+    
+   function diagonalCrossed() returns (bool)
+   {
+       if (game[0][0] == game[1][1] &&
+           game[1][1] == game[2][2])
+           return(true);
+            
+       if (game[0][2] == game[1][1] &&
+           game[1][1] == game[2][0])
+           return(true);
+    
+       return(false);
+   }
+    
+
+    
+    function check_result(uint val) returns (uint){
+        
+        
+       if(rowCrossed()||columnCrossed()||diagonalCrossed())
+		   return 1;
+	   if(noofturns>=9)
+		   return 2;
+	   return 0;
+    }
+    
+    function play_game(uint x, uint y) public number_of_players(){
+        uint result = 0; 
         if(turn == 0)
         {
             //player1
+            game[x][y] = 1;
             turn = 1;
+            noofturns += 1;
+            result = check_result(uint(1));
         }
         else if(turn == 1)
         {
              //player2
+             game[x][y] = 2;
+             turn = 0;
+             noofturns += 1;
+             result = check_result(uint(2));
         }
+        
+        
     }
     
     

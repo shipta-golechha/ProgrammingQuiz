9565054ead0ec62355f7c06b00c39b0724575e83
Parul Jain <32247098+paruljain02@users.noreply.github.com>
2018-11-09 11:30:21 +0530

    Update Tictoe.sol

--- Tictoe.sol
+++ Tictoe.sol
@@ -30,6 +30,11 @@ contract Tictoe
         _;
     }
     
+     modifier check_game(uint x,uint y){
+        require((game[x][y] == 0) ,"It is filled");
+        _;
+    }
+    
     function register_player(){
         
         if(noofplayers == 0){
@@ -90,58 +95,50 @@ contract Tictoe
 	   return 0;
     }
     
-    function play_game(uint x, uint y) public number_of_players()  view returns (string message){
+    function play_game(uint x, uint y) public number_of_players() check_game(x,y) view returns (string message, uint  turnr){
         uint result = 0; 
-        if(turn == 0)
+        if(turn == 0 && msg.sender == player1)
         {
-            if(msg.sender == player1){
+           
             //player1
                 game[x][y] = 1;
-                turn = 1;
+                //turn = 1;
                 noofturns += 1;
                 result = check_result(uint(1));
                 if(result == 1){
-                    return ("Player1 is winner");
+                    return ("Player1 is winner",turn);
                     
                 }
                 else if(result == 2){
-                    return ("Game Draw");
+                    return ("Game Draw",turn);
                 }
-            }
-            else 
-             {
-                return ("player2 turn");
-             }
+        
+           
         }
-        else if(turn == 1)
+        else if(turn == 1 && msg.sender == player2)
         {
-             if( msg.sender == player2){
+             
              //player2
                  game[x][y] = 2;
-                 turn = 0;
+                 //turn = 0;
                  noofturns += 1;
                  result = check_result(uint(2));
                  
                  if(result == 1){
-                    return ("Player2 is winner");
+                    return ("Player2 is winner",turn);
                     
                 }
                 
                 else if(result == 2){
-                    return ("Game Draw");
+                    return ("Game Draw",turn);
                 }
-            }
             
-            else 
-             {
-                return ("player1 turn");
-             }
         }
-        // else 
-        // {
-        //     return ("Not your turn");
-        // }
-       
+        else 
+        {
+            return ("Not your turn",turn);
+        }
+       turn = 1-turn;
     
     }    
     
